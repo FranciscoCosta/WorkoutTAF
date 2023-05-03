62b827431307479323f993358afdb21b8c505134
Xico <francisco100eg@gmail.com>
2023-05-03 16:45:42 -0300

    :Chore: Added react-router depedencie

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.scss';
 import React from 'react';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import { Navbar } from './components';
 
 function App() {
