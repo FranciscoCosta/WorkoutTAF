1e09e371b51137313a04b2882071e86165152eee
Francisco Costa <francisco100eg@gmail.com>
2023-07-26 17:46:19 -0300

    :Feat: Simple structure for login and signup page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+import './Login.scss'
+const Login = () => {
+    return (
+        <div className='Login'>
+            <div className='Login__container'>
+                <h1 className=''>Login</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Login;
