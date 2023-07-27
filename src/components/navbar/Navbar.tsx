11dfb37be6d19d1027834aba02e5aa4391338e15
Francisco Costa <francisco100eg@gmail.com>
2023-07-27 15:16:42 -0300

    :Feat: Added navbar basic style

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-import './Navbar.scss'
+import './Navbar.scss';
+
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div className='Navbar'>Navbar</div>
+    <div className='Navbar'>
+      <div className="Navbar__container">
+        <div className="Navbar__logo-container">
+          <Image src={'/assets/logo-wr.png'} alt="logo-workouttaf" fill className='Navbar__logo-img' />
+          </div>
+          </div>
+          <div className='Navbar__links'>
+            
+          </div>
+    </div>
   )
 }
 
