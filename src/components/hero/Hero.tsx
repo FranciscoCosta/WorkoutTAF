5cf51e7def78acac1ec5b481923534c93a7f7747
ico <francisco100eg@gmail.com>
2023-08-22 13:15:25 -0300

    :Feat: hero section animation

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,25 +1,39 @@
+"use client";
+
 import React from "react";
 import "./Hero.scss";
 
-const Hero = () => {
-  return <div className="Hero">
-    <div className="Hero__container">
-        <div className="Hero__title">
-            <h1>
-              Seja o seu melhor
-            </h1>
-            <p>
-            Descubra o poder transformador de um corpo e mente em sintonia, conquistando seus objetivos fitness de forma única e motivadora.
-            </p>
+import { motion } from "framer-motion";
 
-        <div className="Hero__cto">
-            <button>
-              Inscreva-se
-            </button>
+const Hero = () => {
+  return (
+    <div className="Hero">
+      <div className="Hero__container">
+        <div className="Hero__title">
+          <motion.h1
+            whileInView={{ y: [100, 0], opacity: [0, 1] }}
+            transition={{ duration: 1, delay: 1.3 }}
+          >
+            Seja o seu melhor
+          </motion.h1>
+          <motion.p
+            whileInView={{ scale: [0, 1], opacity: [0, 1] }}
+            transition={{ duration: 1.3, delay: 0 }}
+          >
+            Descubra o poder transformador de um corpo e mente em sintonia,
+            conquistando seus objetivos fitness de forma única e motivadora.
+          </motion.p>
+
+          <div className="Hero__cto">
+            <motion.button
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 1.5, delay: 2.5 }}
+            >Inscreva-se</motion.button>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Hero;
