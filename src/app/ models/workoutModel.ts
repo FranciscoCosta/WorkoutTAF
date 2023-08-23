6c4ac9775ab9e9f7dc606c6bea4bf852fed0fd30
ico <francisco100eg@gmail.com>
2023-08-23 09:23:00 -0300

    :Fix: created a measurementschema and a workout one

--- src/app/ models/workoutModel.ts
+++ src/app/ models/workoutModel.ts
@@ -1,51 +1,24 @@
 import mongoose from "mongoose";
 
-const workoutSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-  },
-  workoutPDF: {
-    type: Buffer, 
-  },
-  diet: {
-    type: Buffer, 
-  },
-  chest : {
-    type: Number,
-    },
-    leftBiceps : {
-    type: Number,
-    },
-    rightBiceps : {
-    type: Number,
-    },
-    waist : {
-    type: Number,
-    },
-    hip : {
-    type: Number,
-    },
-    leftThigh : {
-    type: Number,
-    },
-    rightThigh : {
-    type: Number,
-    },
-    leftCalf : {
-    type: Number,
-    },
-    rightCalf : {
-    type: Number,
-    },
-    weight : {
-    type: Number,
-    },
-    height : {
-    type: Number,
-    },
+const measurementSchema = new mongoose.Schema({
+  chest: Number,
+  leftBiceps: Number,
+  rightBiceps: Number,
+  waist: Number,
+  hip: Number,
+  leftThigh: Number,
+  rightThigh: Number,
+  leftCalf: Number,
+  rightCalf: Number,
+  weight: Number,
+  height: Number,
 });
 
-// This condition is to prevent the model from being compiled again if it was already compiled
-const Workout = mongoose.models.workout || mongoose.model("workout", workoutSchema);
+const workoutSchema = new mongoose.Schema({
+  userId: String,
+  workoutPDF: Buffer,
+  diet: Buffer,
+  measurements: [measurementSchema], // An array of measurement objects
+});
 
-export default Workout;
+const Workout = mongoose.models.workout || mongoose.model("workout", workoutSchema);
